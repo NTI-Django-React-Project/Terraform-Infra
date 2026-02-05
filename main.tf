cd7ab7d744d0f34f9a5e5c558ee40db718047f28
Yassin Abu El-Sheikh <yassinabuelsheikh@gmail.com>
2026-02-05 10:40:21 +0200

    Remove Docker and Jenkins installation script
    Removed the script section for installing Docker and Jenkins.

--- main.tf
+++ main.tf
@@ -238,21 +238,6 @@ locals {
   EOT
 }
 
-<<-SCRIPT
-    #!/bin/bash
-    yum update -y
-    amazon-linux-extras install docker -y
-    systemctl start docker && systemctl enable docker
-    usermod -aG docker ec2-user
-
-    wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-    rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
-    yum install java-11-openjdk -y
-    yum install jenkins -y
-    systemctl start jenkins && systemctl enable jenkins
-  SCRIPT
-}
-
 module "ec2" {
   source = "./modules/ec2"
 
